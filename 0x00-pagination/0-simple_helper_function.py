fb0ad80e9553603d0b3b490e4ef5b3b476c45631
AdonyasG <adonyas2000@gmail.com>
2023-04-20 18:17:24 -0400

    initial commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""simple_helper_fun"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Return the start and end indexes for the current page.
+    Args:
+        page : The current page number (1-indexed).
+        page_size : The number of items to display per page.
+
+    Returns:
+        tuple : A tuple containing the start and end indexes
+        for the current page.
+    """
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    return (start_index, end_index)
