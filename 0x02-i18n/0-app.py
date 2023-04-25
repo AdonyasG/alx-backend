38e5e4f5ada8a1a4c4338c4a4d761c56c4ffb5fa
AdonyasG <adonyas2000@gmail.com>
2023-04-25 08:40:33 -0400

    py fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+"""0-app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
+    """initialize"""
     return render_template('index.html')
