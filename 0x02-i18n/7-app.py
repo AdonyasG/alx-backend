8ddfa3bd4db385b67590f7bc4baf4b94f4795129
AdonyasG <adonyas2000@gmail.com>
2023-04-25 17:34:34 -0400

    advanced task

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""6-app"""
+
+from flask import Flask, render_template, g
+from flask_babel import Babel
+from flask import request
+from typing import Dict
+import pytz
+
+
+class Config(object):
+    """babel config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale"""
+    if request.args.get('locale'):
+        if request.args.get('locale') in app.config['LANGUAGES']:
+            return request.args.get('locale')
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@babel.timezoneselector
+def get_timezone():
+    timezone = request.args.get('timezone')
+    if timezone is not None:
+        try:
+            pytz.timezone(timezone)
+            return timezone
+        except pytz.exceptions.UnknownTimeZoneError:
+            return app.config['BABLE_DEFAULT_TIMEZONE']
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Dict:
+    """getuser"""
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """before request"""
+    usr = get_user()
+    g.usr = usr
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """initialize"""
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
