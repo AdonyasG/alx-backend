3ce98cbd9d0b82a00a73ea45d0778446184793d0
AdonyasG <adonyas2000@gmail.com>
2023-04-25 09:18:34 -0400

    babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""0-app"""
+from flask_babel import Babel
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """babel config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAUlLT_LOCALE = "en"
+    BABEL_DEFAUlLT_TIMEZONE = "UTC"
+
+    @babel.localeselector
+    def get_locale():
+        """get locale"""
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello_world():
+    """initialize"""
+    return render_template('0-index.html')
