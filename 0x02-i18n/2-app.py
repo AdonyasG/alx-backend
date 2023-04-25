2d110b543646786954eb20f24e6d6e2c89d447bd
AdonyasG <adonyas2000@gmail.com>
2023-04-25 10:38:45 -0400

    fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """2-app"""
 from flask_babel import Babel
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 babel = Babel(app)
